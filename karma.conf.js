27f774f55677dfb82da0fc812a1c572f4a90a118
Paige Nash <Paige.Nash@medhost.com>
2019-06-12 08:04:29 -0500

    Clean up karma.conf.js and set loglevel to Debug until further notice

--- karma.conf.js
+++ karma.conf.js
@@ -10,12 +10,11 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'requirejs', 'browserify'],
+    frameworks: ['jasmine', 'requirejs'],
 
 
     // list of files / patterns to load in the browser
     files: [
-      { pattern: 'node_modules/backbone/backbone.js', included: true },
       { pattern: '*.js', included: true },
       { pattern: 'spec/*Spec.js', included: true }
     ],
@@ -28,9 +27,7 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      "spec/*Spec.js": ["browserify"]
-    },
+    preprocessors: {},
 
 
     // test results reporter to use
@@ -49,7 +46,7 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
 
 
     // enable / disable watching file and executing tests whenever any file changes
